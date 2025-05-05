5abf51029b0ec4a45c6ff9d23ecf117668a9dadd
BvanMeijeren <bastiaan.vanmeijeren@hotmail.com>
2025-05-05 23:21:47 +0200

    made sure autocomplete and code running worked according to old setup

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -7,19 +7,24 @@ vim.g.mapleader = " "
 vim.opt.swapfile = false
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 vim.wo.number = true
 
 -- Commentary
-vim.keymap.set('n', '<leader>cc', ':Commentary<CR>', { desc = 'Toggle comment line' })
-vim.keymap.set('v', '<leader>cc', ':Commentary<CR>', { desc = 'Toggle comment selected' })
+vim.keymap.set("n", "<leader>cc", ":Commentary<CR>", { desc = "Toggle comment line" })
+vim.keymap.set("v", "<leader>cc", ":Commentary<CR>", { desc = "Toggle comment selected" })
+
+-- Key mappings for Code Runner
+vim.api.nvim_set_keymap("n", "<leader>rr", ":RunCode<CR>", { noremap = true, silent = true }) -- Run the current file
+vim.api.nvim_set_keymap("n", "<leader>rc", ":RunClose<CR>", { noremap = true, silent = true }) -- Run the current file and close output
+vim.api.nvim_set_keymap("v", "<leader>rs", ":RunCode<CR>", { noremap = true, silent = true }) -- Run selected code
 
 -- sql auto completion
-vim.cmd [[
+vim.cmd([[
   autocmd FileType sql,mysql,plsql setlocal omnifunc=vim_dadbod_completion#omni
-]]
+]])
