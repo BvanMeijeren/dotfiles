22695e62191c127e3ad11a51a3106e75ed08b71d
BvanMeijeren <bastiaan.vanmeijeren@hotmail.com>
2025-05-06 21:37:25 +0200

    removed vim-test and swagger preview modules because I don't use them, added lsps to treesitter, remapped code formatter to leader ff

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,25 +1,32 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = false,
-        ensure_installed = {
-          "bash",
-          "ruby",
-          "html",
-          "css",
-          "scss",
-          "javascript",
-          "typescript",
-          "json",
-          "lua",
-        },
-        highlight = { enable = true },
-        indent = { enable = false },
-      })
-    end
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				ensure_installed = {
+					"bash",
+					"c",
+					"diff",
+					"markdown",
+					"markdown_inline",
+					"query",
+					"vim",
+					"vimdoc",
+					-- "ruby",
+					"html",
+					"css",
+					"scss",
+					-- "javascript",
+					-- "typescript",
+					"json",
+					"lua",
+				},
+				highlight = { enable = true },
+				indent = { enable = false },
+			})
+		end,
+	},
 }
