631fdd4afd069fccabeb36753aae60923168036c
BvanMeijeren <bastiaan.vanmeijeren@hotmail.com>
2025-05-06 20:43:59 +0200

    added new way to escape insert mode, toggleterm now leader+q

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,10 +1,16 @@
+-- vim options
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+-- Set leader key
 vim.g.mapleader = " "
 
 vim.opt.swapfile = false
+
+-- Toggle term
+vim.keymap.set("n", "<leader>q", ":ToggleTerm<CR>")
 
 -- Navigate vim panes better
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
@@ -14,6 +20,9 @@ vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 vim.wo.number = true
+
+-- Assign a new way to escape INSERT mode
+vim.keymap.set("i", "jj", "<Esc>", { noremap = true })
 
 -- Commentary
 vim.keymap.set("n", "<leader>cc", ":Commentary<CR>", { desc = "Toggle comment line" })
