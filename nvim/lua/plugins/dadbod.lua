da1d3f6b209116a86483d869cd5e0389c0f4359d
BvanMeijeren <bastiaan.vanmeijeren@hotmail.com>
2025-05-25 22:49:50 +0200

    updated neovim to use one dadbod file, added ghostty basic config to try it out

--- nvim/lua/plugins/dadbod.lua
+++ nvim/lua/plugins/dadbod.lua
@@ -1,21 +1,38 @@
 return {
-  -- Plugin for database connections
-  {
-    "tpope/vim-dadbod",
-    dependencies = {
-      "kristijanhusak/vim-dadbod-ui", -- UI for managing database interactions
-      "kristijanhusak/vim-dadbod-completion", -- Autocompletion for database queries
-    },
-    config = function()
-      -- Configure vim-dadbod-ui save location
-      vim.g.db_ui_save_location = "~/.config/nvim/db_ui"
-    end,
-    cmd = {
-      "DBUI", "DBUIAddConnection", "DBUIFindBuffer", "DBUIRenameBuffer",
-    },
-    keys = {
-      { "<leader>Du", ":DBUI<CR>", desc = "Open Database UI" },
-    },
-  },
-}
+	{
+		"tpope/vim-dadbod",
+		dependencies = {
+			"kristijanhusak/vim-dadbod-ui",
+			"kristijanhusak/vim-dadbod-completion",
+		},
+		lazy = true,
+		ft = { "sql", "mysql", "plsql" },
+		cmd = {
+			"DBUI",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+			"DBUIRenameBuffer",
+		},
+		keys = {
+			{ "<leader>Du", ":DBUI<CR>", desc = "Open Database UI" },
+		},
+		config = function()
+			vim.g.db_ui_save_location = "~/.config/nvim/db_ui"
+		end,
+	},
 
+	-- Optional: nvim-cmp integration for SQL completion
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"kristijanhusak/vim-dadbod-completion",
+		},
+		opts = function(_, opts)
+			local cmp = require("cmp")
+			opts.sources = cmp.config.sources(vim.tbl_extend("force", opts.sources or {}, {
+				{ name = "vim-dadbod-completion" },
+			}))
+			return opts
+		end,
+	},
+}
